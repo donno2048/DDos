cde1a141d7e00c83aa098cad18e5affc84c551c6
Elisha Hollander <just4now666666@gmail.com>
2021-04-28 19:44:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='DDos',
-    version='1.0.0',
+    version='1.0.1',
     description='DDos any site',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
