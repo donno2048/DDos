8c329c307b9caeeb3e6d10a856e242a683f7d50e
Elisha Hollander <just4now666666@gmail.com>
2021-04-29 10:42:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/donno2048/DDos',
     packages=find_packages(),
+    include_package_data=True,
     license='MIT',
     author='Elisha Hollander',
     classifiers=['Programming Language :: Python :: 3'],
