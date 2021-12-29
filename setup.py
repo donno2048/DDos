7e840c89b471259a851d203e7419bfc0f6c976e3
donno2048 <just4now666666@gmail.com>
2021-12-29 21:22:35 +0200

    add `fake-useragent`

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='DDos',
-    version='1.0.2',
+    version='1.0.3',
     description='DDos any site',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
@@ -10,6 +10,7 @@ setup(
     include_package_data=True,
     license='MIT',
     author='Elisha Hollander',
+    install_requires=['fake-useragent'],
     classifiers=['Programming Language :: Python :: 3'],
     entry_points={ 'console_scripts': [ 'ddos=DDos.__main__:main', 'DDos=DDos.__main__:main' ] }
 )
